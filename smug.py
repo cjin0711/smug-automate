471b1c59985d5b7399bf5d32521652bc1f211945
cjin0711 <139674495+cjin0711@users.noreply.github.com>
2023-12-30 13:10:26 -0500

    fourth commit revised

--- smug.py
+++ smug.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options 
+#from selenium.webdriver.chrome.options import Options 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 options = webdriver.ChromeOptions()
 
@@ -30,7 +31,7 @@ driver.find_element(By.CLASS_NAME, 'sm-login-buttons-container').click()
 
 driver.maximize_window()
 
-#delays element interaction until everything is loaded - explicit wait
+#delays element interaction (up to 10s -> timeout) until everything is loaded - explicit wait
 wait = WebDriverWait(driver, 10)
 upload = wait.until(EC.presence_of_element_located((By.XPATH, "//a[@class='inline-flex dist_button__IZCSb Header_uploadButton__d600D']")))
 upload.click()
@@ -39,9 +40,24 @@ upload.click()
 new_gallery = wait.until(EC.presence_of_element_located((By.XPATH, "//button[@data-testid='uploadoptions_newgallery_button']")))
 new_gallery.click()
 
-#[needs fixing]
-title = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='yui_3_8_0_1_1703913908150_1694']")))
+
+#gallery name (find by ID doesn't work -> find by NAME)
+title = wait.until(EC.presence_of_element_located((By.NAME, "Name")))
 title.send_keys("12.30.2023 Test Gallery")
+
+'''
+#gallery description
+desc = wait.until(EC.presence_of_element_located((By.NAME, "Description")))
+desc.send_keys("Test description for Gallery")
+'''
+
+time.sleep(2)
+
+#gallery creation [needs fixing]
+create = wait.until(EC.presence_of_element_located((By.ID, "yui_3_8_0_1_1703959192564_1699")))
+create.click()
+
+
 
 
 
